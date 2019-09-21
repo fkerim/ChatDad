7c100545afcb517fde2c97ece4898de2a7c0f29e
Fatih Kerim <Fatih.Kerim@logo.com.tr>
2019-09-21 16:52:59 +0300

    dotenv ortam değişken saklayıcıyı kurduk

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const dotenv = require('dotenv'); //ortam değişkenlerinin hazırlanması için
+dotenv.config();
 
-var app = express();
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
+const app = express();
+console.log(process.env.name);
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
